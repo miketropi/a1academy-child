0a4e8089db62b01c9fd8885a694cae4c183c47d6
Hiếu Huỳnh <mike.beplus@gmail.com>
2024-12-12 22:21:14 +0700

    Small update file

--- functions.php
+++ functions.php
@@ -23,38 +23,11 @@
   require_once(A1A_DIR . '/inc/shortcode.php');
 }
 
+add_action( 'wp_enqueue_scripts', 'a1a_parent_enqueue_styles' );
 
-
-// Add custom Theme Functions here
-add_filter('use_block_editor_for_post', '__return_false');
-require 'addon-ux/init.php';
-function hotro_chan_trang(){
-echo do_shortcode('[block id="support-chan-trang"]');	
+function a1a_parent_enqueue_styles() {
+	wp_enqueue_style( 
+		'a1a-parent-style', 
+		get_parent_theme_file_uri( 'style.css' )
+	);
 }
-add_action('wp_footer','hotro_chan_trang');
-function khoahoc(){
-echo 'hiện khóa học';	
-	
-}
-add_shortcode('khoahoc','khoahoc');
-
-
-
-/*Disable edit code & plugin */
-
-
-add_filter('xmlrpc_enabled', '__return_false');
-add_filter('wp_headers', 'wptangtoc_remove_x_pingback');
-add_filter('pings_open', '__return_false', 9999);
-add_filter('pre_update_option_enable_xmlrpc', '__return_false');
-add_filter('pre_option_enable_xmlrpc', '__return_zero');
-function wptangtoc_remove_x_pingback($headers) {
-unset($headers['X-Pingback'], $headers['x-pingback']);
-return $headers;
-}
-
-function wp_version_remove_version() 
-{
-return '';
-}
-add_filter('the_generator', 'wp_version_remove_version');
---
+++ inc/static.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Static
+ */
+
+add_action( 'wp_enqueue_scripts', 'a1a_enqueue_scripts' );
+
+function a1a_enqueue_scripts() {
+  wp_enqueue_style( 'a1a-css', A1A_URI . '/dist/css/a1a.bundle.css', false, A1A_VERSION );
+  wp_enqueue_script( 'a1a-js', A1A_URI . '/dist/a1a.bundle.js', ['jquery'], A1A_VERSION, true );
+
+  wp_localize_script( 'a1a-js', 'A1A_PHP_DATA', [
+    'ajax-url' => admin_url('admin-ajax.php'),
+  ] );
+}
