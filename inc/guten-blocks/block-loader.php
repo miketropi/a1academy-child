668b9ca2d00853f8bd1fe0c1ed9a3cef106f2213
Hiếu Huỳnh <mike.beplus@gmail.com>
2025-01-13 23:43:12 +0700

    add hero section block

--- inc/guten-blocks/block-loader.php
+++ inc/guten-blocks/block-loader.php
@@ -8,4 +8,5 @@
 add_action( 'carbon_fields_register_fields', 'a1a_crb_attach_blocks' );
 function a1a_crb_attach_blocks() {
   require( __DIR__ . '/testimonial-carousel.php' );
+  require( __DIR__ . '/hero-section.php' );
 }
---
+++ inc/guten-blocks/hero-section.php
@@ -0,0 +1,36 @@
+<?php 
+use Carbon_Fields\Block;
+use Carbon_Fields\Field;
+
+Block::make( __( 'Hero Section Block' ) )
+	->add_fields( array(
+    Field::make( 'separator', 'hero_ss_crb_separator', __( '' ) ),
+		Field::make( 'image', 'background_image', __( 'Background Image' ) )
+      ->set_value_type( 'url' ),
+		Field::make( 'image', 'background_image_mobi', __( 'Background Image Mobi' ) )
+      ->set_value_type( 'url' ),
+	) )
+  ->set_category( 'a1a' )
+	->set_keywords( [ __( 'a1a' ), __( 'hero section' ), __( 'content' ) ] )
+  ->set_mode( 'edit' )
+  ->set_inner_blocks( true )
+	->set_inner_blocks_template( array(
+		array( 'core/heading' ),
+		array( 'core/paragraph' ),
+		array( 'core/buttons' )
+	) )
+	->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
+    // print_r($fields);
+    ?>
+		<div class="a1a-hero-section <?php echo isset($attributes['className']) ? $attributes['className'] : ''; ?>">
+      <div class="hero-ss-background-layer-desktop" style="background: url(<?php echo $fields['background_image']; ?>) no-repeat center center / cover;"></div>
+      <div class="hero-ss-background-layer-mobile" style="background: url(<?php echo $fields['background_image_mobi']; ?>) no-repeat center center / cover;"></div>
+      <div class="a1a-hero-section__inner">
+        <div class="hero-ss-entry">
+          <?php echo $inner_blocks; ?>
+        </div>
+        <div class="hero-ss-right"></div>
+      </div>
+    </div>
+		<?php
+	} );
