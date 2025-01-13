c5bc3e603eb2f6ce371adca265648e3d4bc56120
Hiếu Huỳnh <mike.beplus@gmail.com>
2025-01-14 00:01:19 +0700

    Update hero ss

--- inc/guten-blocks/hero-section.php
+++ inc/guten-blocks/hero-section.php
@@ -9,6 +9,8 @@ Block::make( __( 'Hero Section Block' ) )
       ->set_value_type( 'url' ),
 		Field::make( 'image', 'background_image_mobi', __( 'Background Image Mobi' ) )
       ->set_value_type( 'url' ),
+    Field::make( 'checkbox', 'content_overlay_mobi', __( 'Content Overlay Mobi' ) )
+      ->set_option_value( 'no' )
 	) )
   ->set_category( 'a1a' )
 	->set_keywords( [ __( 'a1a' ), __( 'hero section' ), __( 'content' ) ] )
@@ -20,9 +22,9 @@ Block::make( __( 'Hero Section Block' ) )
 		array( 'core/buttons' )
 	) )
 	->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
-    // print_r($fields);
+    print_r($fields);
     ?>
-		<div class="a1a-hero-section <?php echo isset($attributes['className']) ? $attributes['className'] : ''; ?>">
+		<div class="a1a-hero-section <?php echo $fields['content_overlay_mobi'] ? '__content-overlay-mobi' : '' ?> <?php echo isset($attributes['className']) ? $attributes['className'] : ''; ?>">
       <div class="hero-ss-background-layer-desktop" style="background: url(<?php echo $fields['background_image']; ?>) no-repeat center center / cover;"></div>
       <div class="hero-ss-background-layer-mobile" style="background: url(<?php echo $fields['background_image_mobi']; ?>) no-repeat center center / cover;"></div>
       <div class="a1a-hero-section__inner">
