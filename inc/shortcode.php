689c8fe5edf4e4024a1fa424af5e71a1207e7acb
Hiếu Huỳnh <mike.beplus@gmail.com>
2025-02-17 20:14:19 +0700

    Update breadcrumb

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -12,6 +12,8 @@ function a1a_blog_breadcrumb_shortcode() {
     // Start with home link
     $output .= '<div class="a1a-breadcrumb">';
     $output .= '<a href="' . home_url() . '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg> Home</a>';
+
+    $output .= ' / <a href="/blog">Blog</a>';
     
     if (is_single()) {
         // Get post categories
